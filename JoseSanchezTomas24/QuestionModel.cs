54084358f00031280d8924b3c1966c40cafbb2ff
Jose Sanchez <santojo.1991@gmail.com>
2022-05-30 21:35:44 +0200

    Implementamos variables y metodos en la clase QuestionModel

--- JoseSanchezTomas24/QuestionModel.cs
+++ JoseSanchezTomas24/QuestionModel.cs
@@ -11,5 +11,22 @@ namespace JoseSanchezTomas24
         string question;
         string[] answers;
         int value;
+
+        public void questionModel(string ques, string[] answ, int val)
+        {
+           
+        }
+        public string getQuestion()
+        {
+            return this.question;
+        }
+        public string getAnswer()
+        {
+            return this.answers[0];
+        }
+        public int getValue()
+        {
+            return this.value;
+        }
     }
 }
