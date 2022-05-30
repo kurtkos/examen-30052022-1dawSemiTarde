58916b84ed1d2fbd1f7d09a3b347b188f428a098
Jose Sanchez <santojo.1991@gmail.com>
2022-05-30 21:16:16 +0200

    Añadimos los labels del form3

---
+++ JoseSanchezTomas24/Form3.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace JoseSanchezTomas24
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(267, 45);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(232, 16);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Añade una pregunta y sus respuestas";
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Location = new System.Drawing.Point(46, 108);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(61, 16);
+            this.lblPregunta.TabIndex = 1;
+            this.lblPregunta.Text = "Pregunta";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(46, 191);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(73, 16);
+            this.lblRespuesta.TabIndex = 2;
+            this.lblRespuesta.Text = "Respuesta";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(46, 244);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 16);
+            this.lblA.TabIndex = 3;
+            this.lblA.Text = "A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(46, 298);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(17, 16);
+            this.lblB.TabIndex = 4;
+            this.lblB.Text = "B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.Location = new System.Drawing.Point(46, 344);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(17, 16);
+            this.lblC.TabIndex = 5;
+            this.lblC.Text = "C";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(44, 403);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(39, 16);
+            this.lblValor.TabIndex = 6;
+            this.lblValor.Text = "Valor";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblValor;
+    }
+}
